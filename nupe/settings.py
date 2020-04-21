fcaf1c81ebe42014537da5fb45d3d3979b9902fb
Luis Carvalho <luiscvlh11@gmail.com>
2020-04-21 18:02:10 -0300

    Alterar para merjar

--- nupe/settings.py
+++ nupe/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+# Bolsista Luis Carvalho
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
