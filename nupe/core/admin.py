4642728b5f986b6a37b6df5cad0aab72b57f67e9
Luis Carvalho <luiscvlh11@gmail.com>
2020-04-22 19:38:32 -0300

    Adicionar implementacao das tabelas de grau, curso e formacao academica

--- nupe/core/admin.py
+++ nupe/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from nupe.core.models import City, Location, State
+from nupe.core.models import City, State, Location
+from nupe.core.models import Course, Grade, AcademicEducation
 
-location = [City, State, Location]
-
-admin.site.register(location)
+admin.site.register([City, State, Location])
+admin.site.register([Course, Grade, AcademicEducation])
---
+++ nupe/core/models/Course.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="nome")
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="nome")
+    courses = models.ManyToManyField(
+        Course, related_name="grades", related_query_name="grade", through="AcademicEducation", verbose_name="cursos"
+    )
+
+    class Meta:
+        verbose_name = "Grau"
+        verbose_name_plural = "Graus"
+
+    def __str__(self):
+        return self.name
+
+
+class AcademicEducation(models.Model):
+    course = models.ForeignKey(
+        Course,
+        related_name="academics_educations",
+        related_query_name="academic_education",
+        on_delete=models.PROTECT,
+        verbose_name="curso",
+    )
+    grade = models.ForeignKey(
+        Grade,
+        related_name="academics_educations",
+        related_query_name="academic_education",
+        on_delete=models.PROTECT,
+        verbose_name="grau",
+    )
+
+    class Meta:
+        unique_together = ["grade", "course"]
+        verbose_name = "Formação Acadêmica"
+        verbose_name_plural = "Formações Acadêmicas"
+
+    def __str__(self):
+        return "{} em {}".format(self.grade, self.course)
