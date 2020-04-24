4cd3d5800d03645a3fd8265ff5c0cad34ceeda12
Luis Carvalho <luiscvlh11@gmail.com>
2020-04-24 17:58:02 -0300

    Adicionar tabelas de institution, campus, institution_campus, academic_education_campus. #3

--- nupe/core/admin.py
+++ nupe/core/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from nupe.core.models import City, State, Location
-from nupe.core.models import Course, Grade, AcademicEducation
+from nupe.core.models import (
+    City,
+    State,
+    Location,
+    Course,
+    Grade,
+    AcademicEducation,
+    Institution,
+    Campus,
+    InstitutionCampus,
+    AcademicEducationCampus,
+)
 
 admin.site.register([City, State, Location])
 admin.site.register([Course, Grade, AcademicEducation])
+admin.site.register([Institution, Campus, InstitutionCampus, AcademicEducationCampus])
---
+++ nupe/core/models/Institution.py
@@ -0,0 +1,72 @@
+from django.db import models
+from nupe.core.models import Location, AcademicEducation
+
+INSTITUTION_MAX_LENGTH = 50
+CAMPUS_MAX_LENGTH = 50
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=INSTITUTION_MAX_LENGTH, unique=True, verbose_name="nome")
+
+    class Meta:
+        verbose_name = "Instituição"
+        verbose_name_plural = "Instituições"
+
+    def __str__(self):
+        return self.name
+
+
+class Campus(models.Model):
+    name = models.CharField(max_length=CAMPUS_MAX_LENGTH, unique=True, verbose_name="nome")
+    location = models.ForeignKey(Location, related_name="campus", on_delete=models.PROTECT, verbose_name="localização")
+    institutions = models.ManyToManyField(
+        Institution, related_name="campus", through="InstitutionCampus", verbose_name="instituições"
+    )
+    academic_education = models.ManyToManyField(
+        AcademicEducation, related_name="campus", through="AcademicEducationCampus", verbose_name="formação acadêmica"
+    )
+
+    class Meta:
+        verbose_name = "Campus"
+        verbose_name_plural = "Campus"
+
+    def __str__(self):
+        return self.name
+
+
+class InstitutionCampus(models.Model):
+    institution = models.ForeignKey(
+        Institution,
+        related_name="institution_campus",
+        on_delete=models.SET_NULL,
+        verbose_name="instituição",
+        null=True,
+    )
+    campus = models.ForeignKey(Campus, related_name="institution_campus", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ["institution", "campus"]
+        verbose_name = "Instituição - Campus"
+        verbose_name_plural = "Instituições do Campus"
+
+    def __str__(self):
+        return "{} - {}".format(self.institution, self.campus)
+
+
+class AcademicEducationCampus(models.Model):
+    academic_education = models.ForeignKey(
+        AcademicEducation,
+        related_name="course_campus",
+        on_delete=models.SET_NULL,
+        verbose_name="formação acadêmica",
+        null=True,
+    )
+    campus = models.ForeignKey(Campus, related_name="course_campus", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = ["campus", "academic_education"]
+        verbose_name = "Formação Acadêmica - Campus"
+        verbose_name_plural = "Formações Acadêmica do Campus"
+
+    def __str__(self):
+        return "Formação {} ofertada pelo Campus {}".format(self.academic_education, self.campus)
