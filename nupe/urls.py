8e4e796249b57768169b4e206fe691a18a4ca6c9
Luis Carvalho <luiscvlh11@gmail.com>
2020-06-09 16:09:45 -0300

    Finalizar documentação básica da API. #20

--- nupe/urls.py
+++ nupe/urls.py
@@ -5,13 +5,16 @@ from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
 
 from nupe.core.router import router
+from project import description, developer_email, developer_name, developer_social_url
 
-api_info = openapi.Info(title="Nupe API", default_version="v1", description="by Luis Guerreiro",)
-schema_view = get_schema_view(api_info, public=True, permission_classes=[AllowAny],)
+contact = openapi.Contact(name=developer_name, url=developer_social_url, email=developer_email)
+api_info = openapi.Info(title="Nupe API", default_version="v1", description=description, contact=contact)
+
+schema_view = get_schema_view(info=api_info, public=True, permission_classes=[AllowAny])
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
     path("oauth/", include("oauth2_provider.urls", namespace="oauth2_provider")),
-    path("endpoints/", schema_view.with_ui("swagger"), name="endpoints"),
+    path("endpoints/", schema_view.with_ui(renderer="swagger"), name="endpoints"),
 ]
---
+++ project.py
@@ -0,0 +1,7 @@
+# project info
+description = "Projeto desenvolvido pela Fábrica de Software para facilitar os atendimentos do NUPE"
+
+# developer info
+developer_name = "Luis Guerreiro"
+developer_social_url = "https://www.linkedin.com/in/devguerreiro"
+developer_email = "luiscvlh11@gmail.com"
