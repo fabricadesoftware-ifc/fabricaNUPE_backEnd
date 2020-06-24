bec551524aee3b91fa0e845498c4b16b6ddeb58f
Luis Carvalho <luiscvlh11@gmail.com>
2020-06-24 14:39:49 -0300

    Refatorar test de criacao da model de ProfileImage. #27

--- resources/const/datas/ProfileImage.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-VALID_FILE_PROFILE_IMAGE_UPLOAD_PATH = f"{settings.BASE_DIR}/resources/logo_ifc.png"
-INVALID_FILE_PROFILE_IMAGE_UPLOAD_PATH = f"{settings.BASE_DIR}/resources/invalid_file.txt"
--- tests/models/ProfileImage.py
+++ tests/models/ProfileImage.py
@@ -2,18 +2,24 @@ from os import remove
 
 from django.core.files.images import ImageFile
 from django.test import TestCase
+from PIL import Image
 
 from nupe.file.models import ProfileImage
-from resources.const.datas.ProfileImage import VALID_FILE_PROFILE_IMAGE_UPLOAD_PATH
 
 
 class ProfileImageTestCase(TestCase):
     def test_create_valid(self):
-        with open(VALID_FILE_PROFILE_IMAGE_UPLOAD_PATH, "rb") as image:
+        filename = "tests/teste.png"
+
+        new_image = Image.new("RGB", (100, 100), color="blue")  # uma imagem qualquer é criada para teste
+        new_image.save(filename)
+
+        with open(filename, "rb") as image:
             image = ImageFile(image)
 
             profile_image = ProfileImage.objects.create(image=image)
 
-            self.assertEqual(ProfileImage.objects.count(), 1)
+            self.assertEqual(ProfileImage.objects.count(), 1)  # objeto deve ser criado no banco de dados
 
-            remove(profile_image.image.path)
+            remove(profile_image.image.path)  # a midia criada pelo objeto é removida
+            remove(filename)  # a imagem criada para teste é removida
