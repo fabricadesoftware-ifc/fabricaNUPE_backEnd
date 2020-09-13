5cda580d33644f3a0aef259bdfff7dd16b8e528e
beeck <jorge.gabriel.beeck@gmail.com>
2020-09-13 15:59:54 -0300

    Finalizar a tela dos endpoints Person e Student

---
+++ nupe_frontend/components/register/Users/ListUsers.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <label class="title">TODOS OS USERS</label>
+    <b-table :data="data" :columns="columns"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          first_name: "Jesse",
+          last_name: "Simmons",
+        },
+        {
+          id: 2,
+          first_name: "John",
+          last_name: "Jacobs",
+        },
+        {
+          id: 3,
+          first_name: "Tina",
+          last_name: "Gilbert",
+        },
+        {
+          id: 4,
+          first_name: "Clarence",
+          last_name: "Flores",
+        },
+        {
+          id: 5,
+          first_name: "Anne",
+          last_name: "Lee",
+        },
+      ],
+      columns: [
+        {
+          field: "first_name",
+          label: "First Name",
+          searchable: true,
+        },
+        {
+          field: "last_name",
+          label: "Last Name",
+          searchable: true,
+        },
+        {
+          field: "id",
+          label: "ID",
+          width: "100",
+          numeric: true,
+          searchable: true,
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ nupe_frontend/components/register/Users/MainUsers.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box mySpace">
+    <!--Formulario para usuarios-->
+    <CreateUsers />
+    <hr />
+    <ListUsers />
+  </div>
+</template>
+<script>
+import CreateUsers from "@/components/register/Users/CreateUsers.vue";
+import ListUsers from "@/components/register/Users/ListUsers.vue";
+
+export default {
+  components: {
+    CreateUsers,
+    ListUsers,
+  },
+};
+</script>
+<style scoped>
+.mySpace {
+  margin: 2%;
+}
+</style>
