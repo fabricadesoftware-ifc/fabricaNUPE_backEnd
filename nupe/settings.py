28be5f0f03754101bd5e12c087908d440bc47f3c
Eduardo da Silva <eduardo.silva@gmail.com>
2021-06-02 13:55:37 -0300

    Migração de projetos

--- nupe/settings.py
+++ nupe/settings.py
@@ -2,10 +2,11 @@ import os
 from datetime import timedelta
 
 import environ
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 env = environ.Env()
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 
--- nupe_frontend/plugins/global.js
+++
@@ -1,9 +0,0 @@
-export const userKey = "__nupe_systemuser ";
-
-export function showError(e) {
-  for (var item in e.response.data) {
-    this.$toast.error(item + ": " + e.response.data[item]);
-  }
-}
-
-export default { showError, userKey };
